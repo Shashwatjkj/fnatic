627af1a72132020a96c9fd28a547f079ceb8bc1e
Shashwat <anshuphysic07@gmail.com>
2024-05-19 17:34:18 +0530

    new_branch

--- src/middlewares/authcheck.js
+++ src/middlewares/authcheck.js
@@ -6,20 +6,19 @@ import { User } from "../models/user.model.js";
 const verifyJWTtoken=asyncHandler(async(req,res,next)=>{
 try {
     const token=req.cookies?.login_cookie || req.header("Authorization")?.replace("Bearer ","");
-     console.log(!token)
     if(!token){
     
         throw new ApiError(401,"Unauthorized token.");
        
     }
-
+    
     const vaildtoken = await jwt.verify(token,process.env.SECRET_KEY);
     console.log(vaildtoken)
     const user=await User.findById(vaildtoken?._id).select("-password -verifyCode");
     console.log(user)
     if(!user){
         // somthing to do.
-        throw new ApiError(401,"Invalid Access TOken.");
+        throw new ApiError(401,"Invalid Token (Token without _id)");
     }
     
     req.user=user;
---
+++ src/models/product.model.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose"
+
+const productSchema=new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required:true,
+            unique: false
+        },
+        price:{
+            type: Number,
+            require: true   
+        },
+        description:{
+            type: String,
+            required: true
+        },
+        category:{
+            type: String
+        },
+        imageUrl:{
+            type:String
+        }
+        
+        
+    },{timestamps:true}
+)
+
+
+
+
+
+
+export const Product=mongoose.model("Product",productSchema)
