e939366812cce20d6c4925564cc372856beb2f65
Shashwat <anshuphysic07@gmail.com>
2024-06-02 02:23:13 +0530

    co

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -9,7 +9,7 @@ const productSchema=new mongoose.Schema(
         },
         price:{
             type: Number,
-            require: true   
+            required: true   
         },
         description:{
             type: String,
